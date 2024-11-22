1ac68661c71336109ed478f675ddc9fede9d137a
Anastasiia Karpenko <karpenko.anastasiya@gmail.com>
2024-11-22 13:58:29 +0200

    add functionality for likes

--- cat_network/urls.py
+++ cat_network/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 
-from cat_network.views import index, PostListView, PostDetailView, PostCreateView
+from cat_network.views import index, PostListView, PostDetailView, PostCreateView, ToggleLikeView
 
 urlpatterns = [
     path('', index, name='index'),
     path("posts/", PostListView.as_view(), name="post-list"),
     path("posts/<int:pk>/", PostDetailView.as_view(), name="post-detail"),
     path("posts/create/", PostCreateView.as_view(), name="post-create"),
+    path('toggle-like/', ToggleLikeView.as_view(), name='toggle-like'),
 ]
 
 
--- cat_network/views.py
+++ cat_network/views.py
@@ -1,8 +1,9 @@
 from audioop import reverse
 
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404, redirect
 from django.urls import reverse_lazy
+from django.views import View
 from django.views.generic import ListView, DetailView, CreateView
 
 from cat_network.models import Post, Comment, Like, CatUser
@@ -26,6 +27,13 @@ def index(request) -> HttpResponse:
 class PostListView(ListView):
     model = Post
     template_name = "cat_network/post_list.html"
+    queryset = Post.objects.prefetch_related('like_set')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        user_liked_posts = Like.objects.filter(user=self.request.user).values_list('post_id', flat=True)
+        context['user_liked_posts'] = set(user_liked_posts)
+        return context
 
 
 class PostDetailView(DetailView):
@@ -35,6 +43,25 @@ class PostDetailView(DetailView):
 
 class PostCreateView(CreateView):
     model = Post
-    fields = "__all__"
+    fields = ("title", "body", "image")
     success_url = reverse_lazy("cat_network:post-list")
     template_name = "cat_network/post_create.html"
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
+class ToggleLikeView(View):
+    def post(self, request):
+        post_id = request.POST.get('post_id')
+        post = get_object_or_404(Post, id=post_id)
+
+        like, created = Like.objects.get_or_create(user=request.user, post=post)
+
+        if not created:
+            like.delete()
+
+        referer_url = request.META.get('HTTP_REFERER', '/')
+        return HttpResponseRedirect(f"{referer_url}#post-{post.id}")
+
